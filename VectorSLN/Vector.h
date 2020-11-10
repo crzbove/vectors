b682e514c963f7cf36b14264b5ae813608b9928d
Vitaly <crzbove@gmail.com>
2020-11-11 02:08:31 +0300

    Добавлены: конструктор вектора по точкам, скалярное произведение, косинус угла между векторами. Исправлены мелкие ошибки.

--- VectorSLN/Vector.h
+++ VectorSLN/Vector.h
@@ -1,18 +1,51 @@
 #pragma once
 #include <iostream>
 
-class Vector
-{
-private:
-	double* Coords;
-	unsigned int CoordsCount;
-public:
-	Vector(double*, unsigned int);
-	std::string ToString();
-	friend Vector operator+ (const Vector leftOperand, const Vector rightOperand);
-	friend Vector operator* (const Vector leftOperand, const Vector rightOperand);
-	friend Vector operator- (const Vector leftOperand, const Vector rightOperand);
-	friend Vector operator* (const int leftOperand, const Vector rightOperand);
-	friend Vector operator* (const Vector leftOperand, const int rightOperand);
+struct Point {
+	int PointCoordsCount;
+	double* PointCoords;
 };
 
+class Vector
+{
+protected:
+	double* Coords = nullptr;
+	unsigned int CoordsCount = 0;
+public:
+	Vector(double*, unsigned int);
+	Vector(Point, Point);
+	std::string ToString();
+
+	double GetLength();
+
+	bool IsCollinearTo(Vector another);
+	bool IsCodirectedTo(Vector another);
+
+	double FindCOSBetweenThisAnd(Vector another);
+
+	friend Vector operator+ (Vector leftOperand, Vector rightOperand);
+	friend Vector operator- (Vector leftOperand, Vector rightOperand);
+	friend Vector operator* (double leftOperand, Vector rightOperand);
+	friend Vector operator* (Vector leftOperand, double rightOperand);
+
+	// скалярное произведение
+	friend double operator* (Vector leftOperand, Vector rightOperand);
+
+	// векторное произведение
+	friend Vector operator& (Vector leftOperand, Vector rightOperand);
+
+	// проверка равенства строго по определению (см Vector.cpp)
+	friend bool operator== (Vector leftOperand, Vector rightOperand);
+
+	// проверка по длине
+	friend bool operator> (Vector leftOperand, Vector rightOperand);
+
+	// проверка по длине
+	friend bool operator>= (Vector leftOperand, Vector rightOperand);
+
+	// проверка по длине
+	friend bool operator< (Vector leftOperand, Vector rightOperand);
+
+	// проверка по длине
+	friend bool operator<= (Vector leftOperand, Vector rightOperand);
+};
